940f5f4eb10c2cfc8fa4f75a32b5dda901a43d11
rafael <rafael@localhost>
2019-02-11 11:15:41 -0200

    Padronização das páginas html

--- src/main/java/br/mg/gnam/chat/controller/LoginController.java
+++ src/main/java/br/mg/gnam/chat/controller/LoginController.java
@@ -6,9 +6,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * <p>Controller responsável pela tela de login.</p>
+ * 
+ * @author rafael.altagnam
+ * @since 07/02/2019
+ * @version 1.0
+ */
 @Controller
 public class LoginController {
 
+	/**
+	 * Método responsável por direcionar o usuário para a tela de login.
+	 * 
+	 * @param error
+	 * @param logout
+	 * @return
+	 */
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public ModelAndView login(@RequestParam(value = "error", required = false) String error, @RequestParam(value = "logout", required = false) String logout) {
 		ModelAndView model = new ModelAndView();		
